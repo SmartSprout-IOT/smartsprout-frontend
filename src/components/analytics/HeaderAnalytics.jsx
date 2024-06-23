8bf466e0a09f8b451cb91a6323b1062f3f1f307f
Lorenzo Navarro <lorenzo.navaarro@gmail.com>
2024-06-23 13:34:43 -0500

    (feat) analytics screen added

--- src/components/analytics/HeaderAnalytics.jsx
+++ src/components/analytics/HeaderAnalytics.jsx
@@ -5,11 +5,6 @@ export const HeaderAnalytics = () => {
     <div>
       <h1 className='text-2xl font-bold'>Mis Métricas</h1>
       <span className='text-[#5B6776] text-[14px] pt-2'>Revisa tus registros y análisis</span>
-
-
-      <div>
-        <h1>Text goes here</h1>
-      </div>
     </div>
   )
 }
--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -7,15 +7,80 @@ import Box from '@mui/material/Box';
 
 import { HeaderAnalytics } from '../components/analytics/HeaderAnalytics'
 import ReportCard from '../components/card/ReportCard';
+import { LineChartIcon } from 'lucide-react';
+import { LineChart } from '@mui/x-charts';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+
 
 export const Analytics = () => {
   return (
     <div>
       <HeaderAnalytics />
 
+      <div>
+        <h1>Humedad</h1>
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Income chart</InputLabel>
+              <Select
+                labelId="chart-label"
+                id="chart-select"
+                // value={age}
+                label="Income chart "
+                // onChange={handleChange}
+              >
+                {/* <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem> */}
+              </Select>
+          </FormControl>
 
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">This year</InputLabel>
+              <Select
+                labelId="year-label"
+                id="year-select"
+                // value={age}
+                label="This year"
+                // onChange={handleChange}
+              >
+                {/* <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem> */}
+              </Select>
+          </FormControl>
 
-      <div className='flex content-center'>
+          <FormControl fullWidth>
+            <InputLabel id="cultive-label">Select cultive</InputLabel>
+              <Select
+                labelId="cultive-label"
+                id="cultive-select"
+                // value={age}
+                label="Select cultive"
+                // onChange={handleChange}
+              >
+                {/* <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem> */}
+              </Select>
+          </FormControl>
+        </Box>
+      </div>
+
+      <div className='flex justify-center items-center'>
+        <LineChart
+          xAxis={[{ scaleType: 'point', data: ["Enero", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sept", "Oct", "Nov", "Dic"] }]}
+          series={[
+            {
+              data: [200, 400, 120, 780, 350, 760, 180, 960,600,410,540,900],
+            },
+          ]}
+          width={1000}
+          height={400}
+        />
+      </div>
+
+      <div className='flex justify-center items-center'>
         <Grid container rowSpacing={4.5} columnSpacing={2.75}>
           <Grid item xs={12} sx={{mb: -2.25}}>
             <Typography variant='h5'>Reporte de consumo de agua</Typography>
